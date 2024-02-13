faceb04351eae6d979715523c96d455451e489a3
dadada10x <tataleesbk@gmail.com>
2024-02-13 22:57:28 +0700

    Update: styling and add new SVG and font styles

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -10,16 +10,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { createTheme } from '@mui/material/styles';
 import { purple } from '@mui/material/colors';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: '#f44336',
-    },
-  },
-});
 
 
 export default function ButtonAppBar() {
