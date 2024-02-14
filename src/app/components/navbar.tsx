8b333ec2f898d422adf6426a9ae1c163b8e9d459
dadada10x_2nd <tataleesbk@gmail.com>
2024-02-14 17:26:42 +0700

    Create navbar for mobile

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -12,7 +12,8 @@ import { useEffect } from 'react';
 import "@fontsource/roboto/700.css";
 import "/public/font/fontStyle.css";
 import { Avatar } from '@mui/material';
-import { yellow01, black01, yellow02 } from '@/colors';
+import { yellow01, black01, yellow02, white } from '@/colors';
+import { TextField } from '@mui/material';
 import { Margin } from '@mui/icons-material';
 
 
@@ -69,7 +70,7 @@ export default function Navbar() {
 
 
         return (
-            <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{ flexGrow: 0 }}>
                 <AppBar position={"static"}>
                     <Toolbar sx={{ background: yellow01 }}>
                         <Image
@@ -80,27 +81,29 @@ export default function Navbar() {
                             style={{ right: "15px", top: "29px" }}
                         />
                         <form
-                            style={{ width: "50%", height: "40px", marginInline: "50px", border: "0px", fontSize: "15px" }}
+                            style={{ width: "120%", height: "40px", marginInline: "50px", border: "0px", fontSize: "15px" }}
                             onSubmit={(event) => {
                                 event.preventDefault();
                                 // js script to redirect to the search page
                                 const searchValue = (event.target as HTMLFormElement).querySelector('input[name="search"]')?.value;
-                                if (searchValue == null || searchValue === "") { return; }
-                                else
-                                    window.location.href = `/shopping/search/item=${searchValue}`;
+                                if (searchValue === "" || searchValue === null) {
+                                    window.location.href = `/shopping`;
+                                }
+                                else window.location.href = `/shopping/search/item=${searchValue}`;
                             }}
                         >
                             <input
                                 style={{ paddingLeft: "20px", paddingInline: "20px", marginLeft: "20px", borderRadius: "20px", width: "100%", height: "100%", color: black01 }}
                                 type="text"
                                 name="search"
+                                placeholder="Search"
                             />
                         </form>
                         <div style={{ display: "flex", width: "100%", gap: "30px", marginRight: "30px", flexDirection: "row", justifyContent: "right", alignItems: "center" }}>
-                        <IconButton aria-label="profile" size="medium">
-                            <Avatar alt="Profile" src="/picture/profile.png" sx={{ width: 40, height: 40 }} onClick={handleProfileClick} onMouseOver={handleAvatarMouseOver} />
-                        </IconButton>
-                        <AdminButton />
+                            <AdminButton />
+                            <IconButton aria-label="profile" size="medium">
+                                <Avatar alt="Profile" src="/picture/profile.png" sx={{ width: 40, height: 40 }} onClick={handleProfileClick} onMouseOver={handleAvatarMouseOver} />
+                            </IconButton>
                         </div>
                     </Toolbar>
 
@@ -112,22 +115,57 @@ export default function Navbar() {
     // Navbar for Mobile
     function MobileNavbarPage() {
         return (
-            <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{ flexGrow: 0 }}>
                 <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            News
-                        </Typography>
-                        <Button color="inherit">Login</Button>
+                    <Toolbar sx={{ background: yellow01, height: "100px" }}>
+                        <div style={{ display: "flex", width: "120%", gap: "10px", flexDirection: "row", justifyContent: "left", alignItems: "center", marginTop: "25px", height: "30px" }}>
+                            <form
+                                style={{ width: "100%", height: "100%", margin: 0 }}
+                                onSubmit={(event) => {
+                                    event.preventDefault();
+                                    const searchValue = event.target.elements.search.value;
+                                    if (searchValue === "" || searchValue === null) {
+                                        window.location.href = `/shopping`;
+                                    }
+                                    else window.location.href = `/search/item=${searchValue}`;
+                                }}
+                            >
+                                <TextField
+                                    fullWidth
+                                    id="standard-bare"
+                                    variant="outlined"
+                                    style={{ borderRadius: "20px", backgroundColor: "white", width: "100%", height: "100%", color: "black", justifyContent: "center", alignItems: "center", paddingInline: "20px" }}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <IconButton type="submit">
+                                                <Image
+                                                    src={"/picture/search.svg"}
+                                                    alt="search"
+                                                    width={20}
+                                                    height={20}
+                                                />
+                                            </IconButton>
+                                        ),
+                                    }}
+                                    name="search"
+                                    placeholder="Search"
+                                />
+                            </form>
+                        </div>
+
+
+                        <div style={{ display: "flex", width: "20%", marginRight: "10px", flexDirection: "row", justifyContent: "right", alignItems: "center", marginTop: "25px" }}>
+                            <IconButton
+                                size="medium"
+                                edge="start"
+                                color="inherit"
+                                aria-label="profile"
+                                sx={{ width: 20, height: 20 }}
+                            >
+                                <Avatar alt="Profile" src="/picture/profile.png" sx={{ width: 30, height: 30 }} />
+                            </IconButton>
+                        </div>
+
                     </Toolbar>
                 </AppBar>
             </Box>
