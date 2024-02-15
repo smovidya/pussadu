cb320433b36f410a9eef9a9cb88795f67e4287fd
Ratchanon Panmas <qq.panmas@gmail.com>
2024-02-15 17:55:37 +0700

    Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src\app\login\page.tsx"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "noto-sans": ["Noto Sans Thai", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
