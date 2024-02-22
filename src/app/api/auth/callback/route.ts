824c28bb76fe9c564c6e1a828c1bb92fd3d63aae
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-22 23:21:39 +0700

    fix: redirect full form url

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -53,7 +53,7 @@ export async function GET(request: Request) {
   // Use the extracted ticket in your logic or validation
   const { status, message } = await serviceValidation(ticket);
   if (status === 200) {
-    return NextResponse.redirect('https://pussaduvidyacu.vercel.app/')
+    return NextResponse.redirect('https://pussaduvidyacu.vercel.app/profile')
   } else {
     return NextResponse.json({ message: message });
   }
