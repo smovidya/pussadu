de8b066fe647f48ae868c1ec2783dfde95c09133
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-08-06 02:06:43 +0700

    feat: enable remote function

---
+++ src/lib/rpc/hello.remote.ts
@@ -0,0 +1,7 @@
+import { getRequestEvent, query } from "$app/server";
+
+export const hello = query(() => {
+  const { user } = getRequestEvent().locals;
+
+  return `Hello, ${user?.name ?? 'World'}!`;
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+	import { hello } from '$lib/rpc/hello.remote';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1>
+	Rpc:
+	{#await hello()}
+		<span>loading...</span>
+	{:then text}
+		{text}
+	{/await}
+</h1>
