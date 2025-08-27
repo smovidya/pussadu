1afa9919bb218220d81b48a4964b6d17dcd3f757
Patsagorn <patsagorn_y@outlook.com>
2025-08-28 01:46:46 +0700

    fix: update aria-label and title for sidebar toggle button to support localization

--- src/stories/shadcnui/sidebar/sidebar-rail.svelte
+++ src/stories/shadcnui/sidebar/sidebar-rail.svelte
@@ -17,10 +17,10 @@
 	bind:this={ref}
 	data-sidebar="rail"
 	data-slot="sidebar-rail"
-	aria-label="Toggle Sidebar"
+	aria-label="เปิดปิดแถบข้าง"
 	tabIndex={-1}
 	onclick={sidebar.toggle}
-	title="Toggle Sidebar"
+	title="เปิดปิดแถบข้าง"
 	class={cn(
 		'absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear group-data-[side=left]:-right-4 group-data-[side=right]:left-0 after:absolute after:inset-y-0 after:left-[calc(1/2*100%-1px)] after:w-[2px] hover:after:bg-sidebar-border sm:flex',
 		'in-data-[side=left]:cursor-w-resize in-data-[side=right]:cursor-e-resize',
