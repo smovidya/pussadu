0f91c27c30609c98de20a38e113e9afb56683a15
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-08-29 15:30:59 +0700

    forgor this one file

--- src/lib/schema/assetToProject.schema.ts
+++ src/lib/schema/assetToProject.schema.ts
@@ -27,7 +27,8 @@ export const assetToProject = sqliteTable('asset_to_project', {
 	endDate: integer('end_date', {
 		mode: 'timestamp'
 	}).notNull(),
-	...timestamps
+	...timestamps,
+	adminNote: text('note').default(""),
 });
 
 export const assetToProjectRelations = relations(assetToProject, ({ one }) => ({
