ffe183caba7c6c1d09af83a079b0a1f094b28a79
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-08-29 15:28:01 +0700

    feat: add admin's note field

---
+++ drizzle/0004_optimal_longshot.sql
@@ -0,0 +1,23 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_asset_to_project` (
+	`id` text PRIMARY KEY NOT NULL,
+	`asset_id` text NOT NULL,
+	`project_id` text NOT NULL,
+	`borrower_id` text NOT NULL,
+	`amount` integer DEFAULT 1 NOT NULL,
+	`note` text DEFAULT '',
+	`status` text NOT NULL,
+	`start_date` integer NOT NULL,
+	`end_date` integer NOT NULL,
+	`created_at` integer,
+	`updated_at` integer,
+	`deleted_at` integer,
+	FOREIGN KEY (`asset_id`) REFERENCES `asset`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`borrower_id`) REFERENCES `borrower`(`ouid`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_asset_to_project`("id", "asset_id", "project_id", "borrower_id", "amount", "note", "status", "start_date", "end_date", "created_at", "updated_at", "deleted_at") SELECT "id", "asset_id", "project_id", "borrower_id", "amount", "note", "status", "start_date", "end_date", "created_at", "updated_at", "deleted_at" FROM `asset_to_project`;--> statement-breakpoint
+DROP TABLE `asset_to_project`;--> statement-breakpoint
+ALTER TABLE `__new_asset_to_project` RENAME TO `asset_to_project`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
