e755afd1a17f32f29b3a9537a390b40cfcf8f3dc
Patsagorn <patsagorn_y@outlook.com>
2025-09-03 00:26:30 +0700

    feat: add borrowing filter and update schemas for borrowing management

--- src/lib/validator/borrowing.validator.ts
+++ src/lib/validator/borrowing.validator.ts
@@ -1,4 +1,5 @@
 import { assetToProject } from '$lib/schema';
+import { type } from 'arktype';
 import { createInsertSchema } from 'drizzle-arktype';
 
 export const BorrowingRequest = createInsertSchema(assetToProject, {
@@ -16,3 +17,24 @@ export type BorrowingStatus = (typeof BorrowingStatus)['infer'];
 
 export const ReturnStatus = BorrowingStatus.extract('"lost" | "damaged" | "returned"');
 export type ReturnStatus = (typeof ReturnStatus)['infer'];
+
+export const borrowingFilterSchema = type({
+	searchTerm: 'string',
+	statuses:
+		'("lost" | "damaged" | "cancelled" | "pending" | "approved" | "rejected" | "inuse" | "returned")[]',
+	'startDate?': 'Date | undefined',
+	'endDate?': 'Date | undefined',
+	'projectIds?': 'string[]',
+	projectStatus: '("inprogress" | "ended")[]'
+});
+
+export const borrowingUpdateSchema = type({
+	id: 'string',
+	adminNote: 'string | null',
+	amount: 'number | undefined',
+	startDate: 'Date',
+	endDate: 'Date',
+	status:
+		'"lost" | "damaged" | "cancelled" | "pending" | "approved" | "rejected" | "inuse" | "returned"',
+	'+': 'delete'
+});
